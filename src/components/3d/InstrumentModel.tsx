4e8ab047f06d8f5a9a94ea2cdbcdcb738ba89478
jvhnlee <johnlkl906@gmail.com>
2026-07-13 12:12:17 +0800

    fix: remove unused import useFrame

--- src/components/3d/InstrumentModel.tsx
+++ src/components/3d/InstrumentModel.tsx
@@ -1,5 +1,4 @@
 import { useGLTF, Bounds, Center, Clone } from '@react-three/drei';
-import { useFrame } from '@react-three/fiber';
 import { useRef } from 'react';
 import * as THREE from 'three';
 
